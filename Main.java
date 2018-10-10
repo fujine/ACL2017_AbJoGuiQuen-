3aef522434059938b84d45ab3283cc65bdb764ff
Fujine <guillaume.saunier@outlook.fr>
2018-10-10 12:15:25 +0200

    Plateau fait

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
-package ProjetConception;
+package ACL2018_AbJoGuiQuen;
+
+import ACL2018_AbJoGuiQuen.model.Plateau;
 
 public class Main {
     public static void main(String[] args) {
-
+        Plateau p = new Plateau();
+        p.affichageText();
     }
 }
--- model/Plateau.java
+++ model/Plateau.java
@@ -1,4 +1,39 @@
 package ACL2018_AbJoGuiQuen.model;
 
 public class Plateau {
+    private boolean plateau[][];
+    public static int HAUTEUR = 10;
+    public static int LARGEUR = 10;
+
+    public Plateau() {
+        plateau = new boolean[HAUTEUR][LARGEUR];
+        for(int i = 0; i < HAUTEUR; i++) {
+            for(int j = 0; j < LARGEUR; j++) {
+                if(i == 0 || i == HAUTEUR - 1)
+                    plateau[i][j] = false;
+                else {
+                    if(j == 0 || j == LARGEUR-1)
+                        plateau[i][j] = false;
+                    else
+                        plateau[i][j] = true;
+                }
+            }
+        }
+    }
+
+    public boolean estLibre(int posx, int posy) {
+        return plateau[posx][posy];
+    }
+
+    public void affichageText() {
+        for(int i = 0; i < HAUTEUR; i++) {
+            for(int j = 0; j < LARGEUR; j++) {
+                if(plateau[i][j])
+                    System.out.print("X");
+                else
+                    System.out.print("O");
+            }
+            System.out.println();
+        }
+    }
 }
