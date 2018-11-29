ec80299e0c009a2b7e36546ad07c0be68f999c73
abdelaziz111 <azizcherifi10@gmail.com>
2018-11-29 23:00:40 +0100

    "Test"

--- test/model/entites/ChevalierTest.java
+++ test/model/entites/ChevalierTest.java
@@ -52,28 +52,38 @@ public class ChevalierTest {
     }
     
    /*
-    * tester si l'IA du dépalcement du chevalier 
+    * tester l'IA du dépalcement du chevalier 
     */
     
     @Test
     void seRapprocherDeHero() {
-        for (int i = 0; i < 100000; i++) {
-            
+        for (int i = 0; i < 10; i++) {
+        	Jeu mod = Jeu.getInstance();
+          //  mod.getPlateau().addMonstre(c);
             Point coord1 = new Point(5,4);
             Point coord2 = new Point(7,4);
-            Chevalier c = new Chevalier(new Point(coord1),p);
+            Chevalier c = new Chevalier(new Point(coord1),mod.getPlateau());
+           // mod.getPlateau().addMonstre(c);
+          /*  
             Jeu mod = Jeu.getInstance();
+            mod.getPlateau().addMonstre(c);
             Hero h = mod.getHero();
             h.setCoord(coord2);
-           
+            mod.getMonstres().get(0).setCoord(coord1);
             p.setCase(CaseFactory.creerCase(ECase.MUR), new Point(4, 4));
             p.setCase(CaseFactory.creerCase(ECase.MUR), new Point(5, 5));
             p.setCase(CaseFactory.creerCase(ECase.MUR), new Point(5, 3));
-            
+            */
             c.deplacer();
+          /*  
             assertEquals(6, c.getCoord().x);
             assertEquals(4, c.getCoord().y);
+         */
             
+            System.out.println("x="+c.getCoord().x+" y= "+c.getCoord().y);
         }
+        
+        
+        
     }
 }
